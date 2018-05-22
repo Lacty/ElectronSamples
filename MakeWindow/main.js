8496c340f4226e0b9cbe42fe7b919772a27b5f17
Lacty <akira206@gmail.com>
2018-05-22 18:07:35 +0900

    removeコメント

--- MakeWindow/main.js
+++ MakeWindow/main.js
@@ -1,15 +1,6 @@
 
 // モジュールの読み込み
-//const {app, BrowserWindow} = require('electron')
-//const path = require('path')
-//const url = require('url')
-
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
